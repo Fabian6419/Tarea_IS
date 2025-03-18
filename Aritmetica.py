1aed8973b27e57931d86c75ba4b487d330640ee9
Fabian6419 <202113526@est.umss.edu>
2025-03-18 18:36:08 -0400

    se modifico funciones

--- Aritmetica.py
+++ Aritmetica.py
@@ -6,13 +6,21 @@ def factorial_iterativo(n):
         resultado *= i
     return resultado
 
-num = int(input("Ingrese un número: "))
+num = int(input("Ingrese un número para saber el factorial: "))
 print(f"El factorial de {num} es {factorial_iterativo(num)}")
 
 def modulo(a, b):
     if b == 0:
         return "Error: No se puede dividir entre cero"
-    return a % b
+    return a / b
+
+try:
+    a = int(input("Ingrese el primer número para dividir: "))
+    b = int(input("Ingrese el segundo númeroque divide: "))
+    resultado = modulo(a, b)
+    print("El módulo es:", resultado)
+except ValueError:
+    print("Error: Ingrese solo números enteros.")
 
 def raiz_cuadrada(a):
     if a < 0:
@@ -22,7 +30,7 @@ def raiz_cuadrada(a):
 # Validación de entrada
 while True:
     try:
-        num = float(input("Ingrese un número: "))
+        num = float(input("Ingrese un número para saber la raiz cuadrada: "))
         print("Resultado:", raiz_cuadrada(num))
         break  # Salir del bucle si la entrada es válida
     except ValueError:
@@ -31,8 +39,13 @@ while True:
 def valor_absoluto(num):
     return abs(num)
 
-    numero = float(input("Ingresa un número: "))
-    print(f"El valor absoluto de {numero} es {valor_absoluto(numero)}")
-    
+num = float(input("Ingresa un número para calcular su valor absoluto: "))
+print(f"El valor absoluto de {num} es {valor_absoluto(num)}")
+
+
 def suma(a, b):
     return a + b
+
+a = float(input("Ingresa el primer número para la suma: "))
+b = float(input("Ingresa el segundo número para la suma: "))
+print(f"La suma de {a} y {b} es {suma(a, b)}")
