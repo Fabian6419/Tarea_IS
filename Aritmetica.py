e87bb47fb3fa153cc582dadcfbbe0ada55e92574
Fabian6419 <202113526@est.umss.edu>
2025-03-18 06:44:04 -0400

    Agrego funcion de raiz cuadrada

--- Aritmetica.py
+++ Aritmetica.py
@@ -1,3 +1,5 @@
+import math
+
 def factorial_iterativo(n):
     resultado = 1
     for i in range(2, n + 1):
@@ -11,3 +13,17 @@ def modulo(a, b):
     if b == 0:
         return "Error: No se puede dividir entre cero"
     return a % b
+
+def raiz_cuadrada(a):
+    if a < 0:
+        return "No se puede calcular la raíz cuadrada de un número negativo"
+    return math.sqrt(a)
+
+# Validación de entrada
+while True:
+    try:
+        num = float(input("Ingrese un número: "))
+        print("Resultado:", raiz_cuadrada(num))
+        break  # Salir del bucle si la entrada es válida
+    except ValueError:
+        print("Error: Ingrese un número válido.")
